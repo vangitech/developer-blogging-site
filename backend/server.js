5da794607d38e48967a710c8b7e46a68f4854898
Resident Devops <residentdeveloper4@gmail.com>
2026-04-15 03:54:23 +0100

    updated the backend cors issues

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://developer-blogging-site.vercel.app/',
+  origin: 'https://developer-blogging-site.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
